0d393e76ba097af12f553b31ea490162b2e451ab
gregoindev <gregocodes23@gmail.com>
2025-01-29 03:08:03 -0800

    added new prompts

--- ConsoleApp.UserInputDemo/Program.cs
+++ ConsoleApp.UserInputDemo/Program.cs
@@ -1,11 +1,18 @@
 ﻿// declare variables
-string name = string.Empty;
+using System.Xml.Linq;
+
+string firstName = string.Empty;
+string lastName = string.Empty;
 int age = 0;
 int retirementAge = 65;
 
 // prompt user input
-Console.WriteLine("Please enter your name:");
-name = Console.ReadLine();
+Console.WriteLine("Please enter your first name:");
+firstName = Console.ReadLine();
+
+Console.WriteLine("Please enter your last name:");
+lastName = Console.ReadLine();
+
 
 Console.WriteLine("Please enter your age:");
 age = Convert.ToInt32(Console.ReadLine());
@@ -15,6 +22,6 @@ int workingYearsRemaining = retirementAge - age;
 
 
 // output results
-Console.WriteLine($"Full name: {name}");
+Console.WriteLine($"Full name: {firstName} {lastName}");
 Console.WriteLine($"Age: {age}");
 Console.WriteLine($"Working years remaining: {workingYearsRemaining}");
