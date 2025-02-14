eca41575dd4d7699cfe1a41c7cd2e3f7a061bf7d
gregoindev <gregocodes23@gmail.com>
2025-02-14 02:08:23 -0800

    added methods

---
+++ ConsoleApp.Methods/Program.cs
@@ -0,0 +1,85 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to Methods!");
+
+
+// void methods //
+void PrintName()
+{
+    Console.WriteLine(" Full Name");
+}
+
+
+
+
+// value returning //
+int GetFiveYearsAgo() 
+{
+    int year = DateTime.Now.AddYears(-5).Year;
+
+    return year; 
+}
+
+// with parameters //
+void PrintNameWithParams(string name) 
+{
+    Console.WriteLine($"Your name is  {name}");
+}
+
+int GetYearDifferenceWithParams(int year) 
+{
+    int yearDifference = DateTime.Now.Year - year;
+    return yearDifference;
+}
+
+
+int GetFutureOrPastYear(int numberOfYears = 0) 
+{
+    var year = DateTime.Now.AddYears(numberOfYears).Year;
+    return year;
+}
+
+// nullable parameters //
+void PrintNameNullableParam(string? name, int? count)
+{
+    //if (string.IsNullOrEmpty(name))
+    //{
+    //    name = "Default Name";
+    //}
+    //if (!count.HasValue)
+    //{
+    //    count = 1;
+    //}
+    name ??= "Default Name";
+    count ??= 1;
+
+    for (int i = 0; i < count.Value; i++)
+    {
+        Console.WriteLine(name);
+    }
+}
+
+// function calls //
+//PrintName();    
+//int fiveYearsAgo = GetFiveYearsAgo();
+//Console.WriteLine($"Five years ago was : {fiveYearsAgo}");
+
+//Console.WriteLine("Enter your name");
+//string name1 = Console.ReadLine();
+//PrintNameWithParams(name1);
+
+//Console.WriteLine("Enter a year");
+//int pastYear = Convert.ToInt32(Console.ReadLine());
+//int yearsAgo = GetYearDifferenceWithParams(pastYear);
+//Console.WriteLine($"This was {yearsAgo} years ago. ");
+
+//Console.WriteLine("Enter a number of year in the future or past: ");
+//int numberOfYears = Convert.ToInt32(Console.ReadLine());
+
+//var pastYear1 = GetFutureOrPastYear();
+//Console.WriteLine($"The year is: {pastYear1}");
+
+//var pastYear2 = GetFutureOrPastYear(numberOfYears);
+//Console.WriteLine($"The year is : {pastYear2}");
+
+
+PrintNameNullableParam("Gwego", 3);
--- ConsoleApp.Samplecalculator/Program.cs
+++ ConsoleApp.Samplecalculator/Program.cs
@@ -12,15 +12,7 @@ while (numChoice != -1)
 {
     try
     {
-        // Menu
-        Console.Clear();
-        Console.WriteLine("-------------- Welcome to the Sample Calculator --------------");
-        Console.WriteLine("Please select an operation: (-1 to exit) :");
-        Console.WriteLine($"1. Addition");
-        Console.WriteLine($"2. Subtraction");
-        Console.WriteLine($"3. Multiplication");
-        Console.WriteLine($"4. Divison");
-        Console.WriteLine($"5. Fibonacci Sequence");
+        PrintMenu();
         numChoice = Convert.ToInt32(Console.ReadLine());
 
         if (numChoice == -1)
@@ -42,22 +34,19 @@ while (numChoice != -1)
         switch (numChoice)
         {
             case 1:
-                answer = num1 + num2;
+                answer = AddNumbers(num1, num2);
                 break;
             case 2:
-                answer = num1 - num2;
+                answer = SubtractNumbers(num1, num2);
                 break;
             case 3:
-                answer = num1 * num2;
+                answer = MultiplyNumbers(num1, num2);
                 break;
             case 4:
-                answer = num1 / num2;
+                answer = DivideNumbers(num1, num2);
                 break;
             case 5:
-                for (int i = num1; i <= num2; i++)
-                {
-                    answer += i;
-                }
+                answer = Fibonacci(num1, num2);
                 break;
             default:
                 throw new Exception("Invalid Menu Item selected...");
@@ -79,5 +68,52 @@ while (numChoice != -1)
         Console.ReadLine();
     }
 }
-    
+
 Console.WriteLine("End of program");
+
+
+
+// Methods //
+void PrintMenu()
+{
+    // Menu
+    Console.Clear();
+    Console.WriteLine("-------------- Welcome to the Sample Calculator --------------");
+    Console.WriteLine("Please select an operation: (-1 to exit) :");
+    Console.WriteLine($"1. Addition");
+    Console.WriteLine($"2. Subtraction");
+    Console.WriteLine($"3. Multiplication");
+    Console.WriteLine($"4. Divison");
+    Console.WriteLine($"5. Fibonacci Sequence");
+}
+
+int DivideNumbers(int num1, int num2)
+{
+    return num1 / num2;
+}
+
+int MultiplyNumbers(int num1, int num2)
+{
+    return num1 * num2;
+}
+
+int SubtractNumbers(int num1, int num2)
+{
+    return num1 - num2;
+}
+
+int AddNumbers(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+int Fibonacci(int num1, int num2)
+{
+    var answer = 0;
+
+    for (int i = num1; i <= num2; i++)
+    {
+        answer += i;
+    };
+    return answer;
+}
