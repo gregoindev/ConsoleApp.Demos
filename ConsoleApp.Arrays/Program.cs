99c890c0655813049752899841a2a9416de6b7e9
gregoindev <gregocodes23@gmail.com>
2025-02-16 01:38:40 -0800

    arrays and lists practice

---
+++ ConsoleApp.Arrays/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to Arrays ****************************");
+
+
+// Say how many grades / students to be entered
+Console.Write("Indicate the number of grades to be entered: ");
+int numOfGrades = Convert.ToInt32(Console.ReadLine());
+
+int[] grades = new int[numOfGrades];
+string[] students = new string[numOfGrades];
+
+
+
+for (int i = 0; i < numOfGrades; i++)
+{
+    Console.Write("Enter Student's name: ");
+    students[i] = Console.ReadLine();
+
+    Console.Write("Enter Grade: ");
+    grades[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("The grades you have entered are: ");
+for (int i = 0; i < numOfGrades; i++)
+{
+    Console.WriteLine($"{students[i]}: {grades[i]}");
+}
+
+
+
+
+
+//string[] studentNames = { };
+
+//for (int i = 0; i < studentNames.Length; i++)
+//{
+//    Console.Write($"Write Student name: ");
+//    studentNames[i] = Console.ReadLine();
+//}
+
+//for (int i = 0; i < studentNames.Length; i++)
+//{
+//    Console.WriteLine(studentNames[i]);
+//}
+
+
+
---
+++ ConsoleApp.Lists/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+// **************** LISTS
+
+List<int> grades = new List<int>();
+List<string> students = new List<string>();
+int grade = 0;
+string name;
+int gradeCount = 0;
+int @continue;
+//var grades = new List<int>();
+//List<int> grades = new();
+
+//grades.Add(45);
+//Console.WriteLine(grades);
+
+do
+{
+    gradeCount+= 1;
+    Console.Write("Enter Student's name: ");
+    name = Console.ReadLine();
+    students.Add(name);
+
+    Console.Write("Enter grade: ");
+    grade = Convert.ToInt32(Console.ReadLine());
+    grades.Add(grade);
+
+    Console.Write("Do you wish to continue? (1 = yes | 2 = no): ");
+    @continue = Convert.ToInt32(Console.ReadLine());
+
+  
+} while (@continue == 1);
+
+//Console.WriteLine("Printing grades using for loop");
+//for (int i = 0; i < grades.Count; i++) 
+//{
+//    Console.WriteLine(grades[i]);
+//}
+Console.WriteLine("The grades you have entered are:");
+for (int i = 0; i < gradeCount; i++) 
+{
+    Console.WriteLine($"{students[i]}: {grades[i]}");
+}
+
+//// foreach loop
+//Console.WriteLine("Printing grades using foreach");
+//foreach (int g in grades) 
+//{
+//    Console.WriteLine(g);
+//}
